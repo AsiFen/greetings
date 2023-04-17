2ff38655d6e0214f0fd528eea7db2e89b71ca27f
Asisipho <missfente@gmail.com>
2023-04-17 23:13:33 +0200

    added localStorage, its not working

--- greet.js
+++ greet.js
@@ -1,27 +1,41 @@
 //referencing my html elements
 var nameElement = document.querySelector('.name');
 var btnGreet = document.querySelector('.greetBtn');
-var displayElement = document.querySelector('.display');
+var displayGreeting = document.querySelector('.display');
 
-// query select the checked radio button
+//query select the output for numeber of greetings
+var displayGreetCount = document.querySelector('.countGreets')
+
+
 var greeting = ''
+// counter variable
+let count = 0
 // function that greets a user with their name and hello
 function greet() {
+    // query select the checked radio button
     var languageElement = document.querySelector('input[class="language"]:checked');
     var name = nameElement.value;
     if (languageElement) {
         var checkedLanguage = languageElement.value;
         if (checkedLanguage == 'english') {
             greeting = 'Hello, ' + name
+            count++
         }
         if (checkedLanguage == 'swahili') {
             greeting = 'Jambo, ' + name
+            count++
+
         }
         if (checkedLanguage == 'isiXhosa') {
             greeting = 'Molo, ' + name
+            count++
+
         }
     }
-    displayElement.innerHTML = greeting;
+    localStorage.setItem('count',count)
+    console.log(localStorage.getItem('count'));
+    displayGreetCount.innerHTML = JSON.parse(localStorage.getItem('count'));
+    displayGreeting.innerHTML = greeting;
 }
 //when btnGreet is clicked my function must be executed
 btnGreet.addEventListener('click', greet)
