b3ca0dbd63348fbac8a5d4856f116a5cf6141ecb
Asisipho <missfente@gmail.com>
2023-04-17 21:00:15 +0200

    greets user in 3 different languages

--- greet.js
+++ greet.js
@@ -3,10 +3,24 @@ var nameElement = document.querySelector('.name');
 var btnGreet = document.querySelector('.greetBtn');
 var displayElement = document.querySelector('.display');
 
+// query select the checked radio button
+var greeting = ''
 // function that greets a user with their name and hello
-function greet(){
+function greet() {
+    var languageElement = document.querySelector('input[class="language"]:checked');
     var name = nameElement.value;
-    var greeting =  'Hello, '+ name;
+    if (languageElement) {
+        var checkedLanguage = languageElement.value;
+        if (checkedLanguage == 'english') {
+            greeting = 'Hello, ' + name
+        }
+        if (checkedLanguage == 'swahili') {
+            greeting = 'Jambo, ' + name
+        }
+        if (checkedLanguage == 'isiXhosa') {
+            greeting = 'Molo, ' + name
+        }
+    }
     displayElement.innerHTML = greeting;
 }
 //when btnGreet is clicked my function must be executed
